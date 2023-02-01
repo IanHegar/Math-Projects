12ab6a68c06b01f10b83e1860ecda08d7bec4528
Ian <123431432+IanHegar@users.noreply.github.com>
2023-02-01 19:55:51 -0300

    Program to show the first 10 terms of an AP

---
+++ arithmetic_progression.py
@@ -0,0 +1,16 @@
+# Arithmetic Progression Generator
+count = 1
+
+print("-=" * 40)
+firstTerm = int(input("Enter first term: "))
+ratio = int(input("Enter ratio: "))
+
+# Only the first 10 terms of the progression
+while True:
+    if count != 10:
+        print(f"{firstTerm} → ", end="")
+        firstTerm += ratio
+        count += 1
+    else: 
+        print(f"{firstTerm} → End")
+        break
